ee9e81434b0c602c609eb8eb70868364ef82732d
Yurii Kozlov <yurka22870@gmail.com>
2023-03-28 12:35:39 +0300

    code styling was improved

--- components/SignUpContainer/SignUpContainer.tsx
+++ components/SignUpContainer/SignUpContainer.tsx
@@ -1,12 +1,12 @@
 import React, { ReactElement, ReactNode } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { v4 as uuid_v4 } from 'uuid';
 import { SignUp } from 'types/SignUp';
 import { SignUpFooterListItem } from 'components/SignUpContainer/SignUpFooterListItem';
 import { Error } from 'components/Error';
 import { Container } from 'components/Container';
 import styles from 'components/SignUpContainer/SignUpContainer.module.scss';
-import Link from 'next/link';
 
 type SignUpContainerProps = {
   children: ReactNode,
@@ -30,7 +30,7 @@ export const SignUpContainer: React.FC<SignUpContainerProps> = ({ children, sign
                   height={40}
                   src={images?.netflixLogo}
                   width={184}
-              />
+                />
               </Link>
             }
             <Link className={styles.signOutLink} href="#">Sign In</Link>
